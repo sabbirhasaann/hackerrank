2186671661b005085af0a9276ebfa83ca3d20977
sabbirhasaann <sabbirhasan9989@gmail.com>
2025-11-03 19:37:52 +0600

    Mutations.py

---
+++ Python/Mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    s = list(string)
+    s[position] = character
+    s1 = "".join(s)
+    return s1
+
+
+if __name__ == "__main__":
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
